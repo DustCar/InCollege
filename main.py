c0f4a5fe41b41e489501e6373fb242650a617d63
DustCar <93010652+DustCar@users.noreply.github.com>
2023-06-05 18:40:22 +0000

    imported account to main and added createaccount() in createAcctPage()

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 """This file contains the main code for InCollege,
 including the different pages and relevant functions."""
 
-
 # imports
-import sqlite3
+import account
 import utility
-
 
 # global variables
 minUsr = 1
@@ -17,28 +15,30 @@ maxPasswd = 12
 # function for initial InCollege screen
 def inCollege():
   utility.pageTitle("Welcome to InCollege!")
-  
-  welcome = {"Log In": login, 
-             "Create An Account": createAcct, 
-             "Exit InCollege": closeApp}
-  
+
+  welcome = {
+    "Log In": login,
+    "Create An Account": createAcctPage,
+    "Exit InCollege": closeApp
+  }
+
   utility.printMenu(welcome)
-  
+
   option = input("Input: ")
   optionNum = utility.choiceValidation(option)
 
   utility.call(optionNum, welcome)
   return
-  
+
 
 # function for log in page
 def login():
   utility.pageTitle("Log In")
-  
+
   usr = input("Username: ")
   passwd = input("Password: ")
   loginAuthorization(usr, passwd)
-  
+
   utility.printMessage("You have successfully logged in")
   loggedin()
   return
@@ -48,7 +48,7 @@ def login():
 def loginAuthorization(usr, passwd):
   badUsr = len(usr) < minUsr or len(usr) > maxUsr
   badPasswd = len(passwd) < minPasswd or len(passwd) > maxPasswd
-  
+
   while badUsr or badPasswd:
     utility.printMessage("Incorrect username/password, please try again")
     end = input("Press Q to quit. Otherwise, press any key.\nInput: ")
@@ -65,17 +65,19 @@ def loginAuthorization(usr, passwd):
 # function for screen after logging in
 def loggedin():
   utility.pageTitle("Home")
-  
-  home = {"Job Search" : utility.construction,
-          "Find Someone" : utility.construction,
-          "Learn a New Skill" : learnSkill,
-          "Log out" : inCollege}
+
+  home = {
+    "Job Search": utility.construction,
+    "Find Someone": utility.construction,
+    "Learn a New Skill": learnSkill,
+    "Log out": inCollege
+  }
 
   utility.printMenu(home)
 
   option = input("Input: ")
   optionNum = utility.choiceValidation(option)
-  
+
   utility.call(optionNum, home)
   return
 
@@ -84,30 +86,33 @@ def loggedin():
 def closeApp():
   utility.printMessage("Exited InCollege.")
   exit(0)
-  
+
 
 # function for creating an account -- INCOMPLETE
-def createAcct():
+def createAcctPage():
   utility.pageTitle("Create An Account")
+  account.createAccount()
   return
 
 
 # function to learn a skill
 def learnSkill():
   utility.pageTitle("Learn a New Skill")
-  
-  skills = {"Skill1" : utility.construction,
-            "Skill2" : utility.construction,
-            "Skill3" : utility.construction,
-            "Skill4" : utility.construction,
-            "Skill5" : utility.construction,
-            "Home" : loggedin}
-  
+
+  skills = {
+    "Skill1": utility.construction,
+    "Skill2": utility.construction,
+    "Skill3": utility.construction,
+    "Skill4": utility.construction,
+    "Skill5": utility.construction,
+    "Home": loggedin
+  }
+
   utility.printMenu(skills)
 
   option = input("Input: ")
   optionNum = utility.choiceValidation(option)
-  
+
   utility.call(optionNum, skills)
   return
 

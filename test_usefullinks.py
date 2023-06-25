0e638b5387636ed982a61154815a4c5976d2f44c
DustCar <93010652+DustCar@users.noreply.github.com>
2023-06-25 21:36:03 +0000

    test_usefullinks.py: completed useful links test file

--- test_usefullinks.py
+++ test_usefullinks.py
@@ -1,5 +1,5 @@
 import pytest
-import usefullinks
+import usefullinks, generallinks
 
 from unittest import mock
 from unittest.mock import patch
@@ -31,6 +31,39 @@ def database():
 
 	# Tear down the database after testing
 	conn.close()
+
+def test_UsefulLinksPage(capfd, monkeypatch):
+  # test if necessary links are displayed
+  monkeypatch.setattr('builtins.input', lambda _: '5')
+  result = usefullinks.UsefulLinksPage()
+  out, err = capfd.readouterr()
+  assert "Press 1 for General." in out
+  assert "Press 2 for Browse InCollege." in out
+  assert "Press 3 for Business Solutions." in out
+  assert "Press 4 for Directories." in out
+  assert "Press 5 for Back." in out
+
+  # test if General link goes to general page
+  inputs = iter(['1', '8', '5'])
+  monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+  result = usefullinks.UsefulLinksPage()
+  out, err = capfd.readouterr()
+  assert "Press 1 for Sign Up." in out
+  assert "Press 2 for Help Center." in out
+  assert "Press 3 for About." in out
+  assert "Press 4 for Press." in out
+  assert "Press 5 for Blog." in out
+  assert "Press 6 for Careers." in out
+  assert "Press 7 for Developers." in out
+  assert "Press 8 for Back." in out
+
+  # test if next three links print under construction (will only test one to cover for all under construction functions)
+  inputs = iter(['2', '1', '5'])
+  monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+  result = usefullinks.UsefulLinksPage()
+  out, err = capfd.readouterr()
+  assert "Under Construction." in out
+  
 # Run the tests
 if __name__ == '__main__':
 	pytest.main()
